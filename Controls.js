4dc23daf94d3ce1be2817c601f8ba31f57c2efa8
Shveytcer Roman <truelive4@gmail.com>
2018-06-10 13:56:35 +0300

    Инициализация Ralph

--- Controls.js
+++ Controls.js
@@ -12,11 +12,25 @@ class Point{
     }
 }
 
+class Color{
+    constructor(R,G,B){
+        this.R = R;
+        this.G = G;
+        this.B = B;
+    }
+    ToString(){
+        return "rgb("+this.R+", "+this.G+", "+this.B+")";
+    }
+}
+
 class Controll{
     constructor(size){
         this.size = size;
         this.position = new Point(0, 0);
-        this.graphics = createGraphics(size.Width, size.Height);
+    }
+
+    SetDisplayObject(paper){
+
     }
     SetPosition(point){
         this.position = point;
@@ -24,45 +38,28 @@ class Controll{
     GetSize(){
         return this.size;
     }
+    onClick(){} 
     OnDraw(){}
     draw(){
         this.OnDraw();
         image(this.graphics, this.position.x, this.position.y);
     }
-    isPointIn(point){
-        var pointDiff_X = point.x - this.position.x;
-        var pointDiff_Y = point.y - this.position.y;
-        if(pointDiff_X < 0 || pointDiff_Y < 0){
-            return false
-        }
-        if(pointDiff_X>this.size.Width || pointDiff_Y>this.size.Height){
-            return false;
-        }
-        return true;
-
+    onClickEvent(){
+        onClick();
     }
-    onClick(){}
+    
 }
 
 class Button extends Controll{
     constructor(size, label){
         super(size);
         this.label = label;
-        this.drawGraphics = function (){
-            this.graphics.textSize(12);
-            this.graphics.rect(0,0,this.size.Width-1, this.size.Height-1);
-            this.graphics.textAlign(CENTER,CENTER);
-            var textMargin = 3;
-            this.graphics.text(this.label,textMargin,textMargin,this.size.Width-textMargin, this.size.Height-textMargin);
-        };
-        this.drawGraphics();
     }
-    onClick(){
-        this.label = this.label+"1";
-        this.drawGraphics();
-    }
-    OnDraw(){
-
+    SetDisplayObject(paper){
+        this.graphics = paper.rect(this.position.x, this.position.y,this.size.Width,this.size.Height);
+        var bg = new Color(193,232,190)
+        this.graphics.attr({fill:bg.ToString()});
+        this.graphics.click(this.onClickEvent());
     }
 }
 
--- initRalph.js
+++ initRalph.js
@@ -1,10 +1,13 @@
 
 $(function() {
-    var paper = Raphael(document.getElementById("paper"),600,600);
+    var body = document.getElementById("paper") 
+    var paper = Raphael(body);
     paper.circle(0,0,50);
-    paper.rect(0, 0, 50, 25);
-    $('div#paper').click(function( event ){
+    
+    var butt = new Button(new Size(100,25));
+
+    /*$('div#paper').click(function( event ){
         paper.rect(event.offsetX, event.offsetY, 50, 25);
-      });
+      });*/
 })
 
